a7d5103ff5828ffa3ae6ba3dd150411c3a4ded79
dylan h <baolong281@gmail.com>
2024-07-30 15:31:14 -0500

    post endpoint

--- src/_components/pantry/create.tsx
+++ src/_components/pantry/create.tsx
@@ -11,8 +11,34 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import ImageUploader from "./imageUpload";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export function CreatePantryItem() {
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    const res = await fetch("/api/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        image: "",
+        quantity: Number(quantity),
+        username: "dylan",
+      }),
+    });
+
+    if (res.ok) {
+      router.refresh();
+    }
+  };
+
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("1");
+
   return (
     <Dialog>
       <DialogTrigger asChild className="h-13 w-1/6">
@@ -35,17 +61,29 @@ export function CreatePantryItem() {
             <Label htmlFor="name" className="text-right">
               Item Name
             </Label>
-            <Input id="name" value="" className="col-span-3" />
+            <Input
+              id="name"
+              className="col-span-3"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right">
               Quantity
             </Label>
-            <Input id="username" value="" className="col-span-3" />
+            <Input
+              id="username"
+              className="col-span-3"
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Add Item</Button>
+          <Button type="submit" onClick={handleSubmit}>
+            Add Item
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
---
+++ src/app/api/items/route.ts
@@ -0,0 +1,72 @@
+// pages/api/createItem.ts
+import { db } from "@/server/db";
+import { items, users } from "@/server/db/schema";
+import { and, eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  console.log(req.body);
+  // read the request body which is json
+  const body = await req.json();
+  const { name, image, quantity, username } = body;
+
+  const user = await db
+    .select()
+    .from(users)
+    .where(eq(users.name, username))
+    .execute();
+
+  if (user.length === 0) {
+    return new NextResponse(JSON.stringify({ error: "User does not exist" }), {
+      status: 400,
+    });
+  }
+
+  const item = await incrementItem(name, quantity, user[0]!.id);
+
+  if (item) {
+    return new NextResponse(JSON.stringify(item), { status: 200 });
+  }
+
+  try {
+    const newItem = await db
+      .insert(items)
+      .values({
+        name: name,
+        image: image,
+        quantity: quantity,
+        userId: user[0]!.id,
+      })
+      .returning();
+
+    return new NextResponse(JSON.stringify(newItem), { status: 200 });
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ error: "Failed to create item" }),
+      {
+        status: 500,
+      },
+    );
+  }
+}
+
+async function incrementItem(name: string, quantity: number, userId: number) {
+  const item = await db
+    .select()
+    .from(items)
+    .where(and(eq(items.name, name), eq(items.userId, userId)))
+    .execute();
+
+  if (item.length === 0) {
+    return null; // Return null or any appropriate response if item is not found
+  }
+
+  const updatedItem = await db
+    .update(items)
+    .set({ quantity: item[0]!.quantity + quantity })
+    .where(eq(items.name, name))
+    .returning()
+    .execute();
+
+  return updatedItem;
+}
